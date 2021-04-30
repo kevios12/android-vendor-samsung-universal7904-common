ae4e83a66e80d92fd483057c673eac28a37aa6d3
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-04-30 15:19:23 +0200

    Update universal7904-common-vendor.mk
    removed PRODUCT_COPY_FILES > changed in Android.bp

--- universal7904-common-vendor.mk
+++ universal7904-common-vendor.mk
@@ -16,23 +16,3 @@
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/samsung/universal7904-common
-
-PRODUCT_COPY_FILES += \
-    vendor/samsung/universal7904-common/proprietary/lib/hw/audio.playback_record.default.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/audio.playback_record.default.so \
-    vendor/samsung/universal7904-common/proprietary/lib/libHMT.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libHMT.so \
-    vendor/samsung/universal7904-common/proprietary/lib/lib_SoundAlive_3DPosition_ver202.so:$(TARGET_COPY_OUT_SYSTEM)/lib/lib_SoundAlive_3DPosition_ver202.so \
-    vendor/samsung/universal7904-common/proprietary/lib/lib_SoundAlive_AlbumArt_ver105.so:$(TARGET_COPY_OUT_SYSTEM)/lib/lib_SoundAlive_AlbumArt_ver105.so \
-    vendor/samsung/universal7904-common/proprietary/lib/lib_SoundAlive_play_plus_ver210.so:$(TARGET_COPY_OUT_SYSTEM)/lib/lib_SoundAlive_play_plus_ver210.so \
-    vendor/samsung/universal7904-common/proprietary/lib/lib_SoundBooster_ver950.so:$(TARGET_COPY_OUT_SYSTEM)/lib/lib_SoundBooster_ver950.so \
-    vendor/samsung/universal7904-common/proprietary/system_ext/apex/com.android.vndk.v29/lib/libcamera_client.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/apex/com.android.vndk.v29/lib/libcamera_client.so \
-    vendor/samsung/universal7904-common/proprietary/lib/libprofileparamstorage.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libprofileparamstorage.so \
-    vendor/samsung/universal7904-common/proprietary/lib/libsecaudiocoreutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsecaudiocoreutils.so \
-    vendor/samsung/universal7904-common/proprietary/lib64/hw/audio.playback_record.default.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/hw/audio.playback_record.default.so \
-    vendor/samsung/universal7904-common/proprietary/lib64/libHMT.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libHMT.so \
-    vendor/samsung/universal7904-common/proprietary/lib64/lib_SoundAlive_3DPosition_ver202.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/lib_SoundAlive_3DPosition_ver202.so \
-    vendor/samsung/universal7904-common/proprietary/lib64/lib_SoundAlive_AlbumArt_ver105.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/lib_SoundAlive_AlbumArt_ver105.so \
-    vendor/samsung/universal7904-common/proprietary/lib64/lib_SoundAlive_play_plus_ver210.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/lib_SoundAlive_play_plus_ver210.so \
-    vendor/samsung/universal7904-common/proprietary/lib64/lib_SoundBooster_ver950.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/lib_SoundBooster_ver950.so \
-    vendor/samsung/universal7904-common/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so \
-    vendor/samsung/universal7904-common/proprietary/lib64/libprofileparamstorage.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libprofileparamstorage.so \
-    vendor/samsung/universal7904-common/proprietary/lib64/libsecaudiocoreutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsecaudiocoreutils.so
